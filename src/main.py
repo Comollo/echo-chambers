ffbc5caf88ef5f53293c823d990e072d543e580f
Comollo <comottofederico@gmail.com>
2019-12-26 12:29:23 +0100

    Tested controversy measure

--- src/main.py
+++ src/main.py
@@ -4,19 +4,19 @@ import sys
 import networkx as nx
 from src.common.utility import get_filename, border_msg
 from src.community.partition import CommunityDetection
-
+from src.controversy.measures import RandomWalkControversy
 
 try:
     path = sys.argv[1]
     filename = get_filename(path)
 
     print("Creating graph for {}".format(filename))
-
-    graph = nx.read_weighted_edgelist(path, delimiter="\t")
-
+    graph = nx.read_weighted_edgelist(path, delimiter=",")
     print("Graph created")
 
     communities = CommunityDetection(graph=graph, algorithm="fluidc")
+
+    controversy = RandomWalkControversy(graph=graph, communities=communities.communities)
     # G = nx.read_edgelist(file, delimiter='\t')
 except Exception as e:
     print("An error occurred: {}".format(e))
