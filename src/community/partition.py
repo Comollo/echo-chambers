886e4b2474607f81c13896a9280da33a38f18724
Comollo <comottofederico@gmail.com>
2019-12-31 16:48:21 +0100

    Refactoring

--- src/community/partition.py
+++ src/community/partition.py
@@ -1,22 +1,22 @@
 # define function to partition a graph
-from typing import Dict
 
 import networkx as nx
 from enum import Enum, unique
 from networkx.algorithms import community
 from src.common.utility import print_element
+from typing import Dict
 
 
 @unique
 class Algorithm(Enum):
 
-    KERNIGHAN = "KERNIGHAN-LIN"
+    KERNIGHAN_LIN = "KERNIGHAN-LIN"
     FLUIDC = "FLUIDC"
 
 
 class CommunityDetection:
 
-    algorithms = [Algorithm.KERNIGHAN.value,
+    algorithms = [Algorithm.KERNIGHAN_LIN.value,
                   Algorithm.FLUIDC.value]
 
     def __init__(self, graph: nx.Graph, algorithm: str, k: int = 2):
@@ -35,13 +35,13 @@ class CommunityDetection:
 
         communities: Dict[int, set] = dict()
 
-        if self.algorithm.upper() == Algorithm.KERNIGHAN.value:
+        if self.algorithm.upper() == Algorithm.KERNIGHAN_LIN.value:
             partitions = community.kernighan_lin_bisection(self.graph)
             for p in range(len(partitions)):
                 communities[p] = partitions[p]
 
         elif self.algorithm.upper() == Algorithm.FLUIDC.value:
-            partitions = community.asyn_fluidc(self.graph, k)
+            partitions = community.asyn_fluidc(self.graph, k, seed=10)
             for p in range(k):
                 communities[p] = next(partitions)
 
