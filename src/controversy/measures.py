19722f437ddc65872e2f4050914ad63d71d98a31
Comollo <comottofederico@gmail.com>
2020-01-04 11:39:31 +0100

    Bug fixing

--- src/controversy/measures.py
+++ src/controversy/measures.py
@@ -1,4 +1,4 @@
-# define controversy measure
+# define controversy measures
 
 import networkx as nx
 import random
@@ -8,8 +8,9 @@ from src.common.utility import border_msg, lists_to_dict
 
 class RandomWalkControversy(ControversyMeasure):
 
-    def __init__(self, graph: nx.Graph, communities: dict, percent: float = 0.10):
+    def __init__(self, graph: nx.Graph, communities: dict, iteration: int = 1000, percent: float = 0.10):
 
+        self.iteration = iteration
         self.percent = percent
         super().__init__(graph, communities)
 
@@ -21,26 +22,20 @@ class RandomWalkControversy(ControversyMeasure):
         left = list(self.communities[0])
         right = list(self.communities[1])
 
-        left_values = [1] * len(left)
-        dict_left = lists_to_dict(left, left_values)
-
-        right_values = [1] * len(right)
-        dict_right = lists_to_dict(right, right_values)
-
         left_left = 0
         left_right = 0
         right_right = 0
         right_left = 0
 
-        left_percent = int(percent*len(dict_left.keys()))
-        right_percent = int(percent*len(dict_right.keys()))
+        left_percent = int(percent*len(left))
+        right_percent = int(percent*len(right))
 
-        for j in range(1000):
+        for j in range(self.iteration):
 
             print("Random Walking -> round {}".format(j))
 
-            user_nodes_left = self.get_random_nodes_from_labels(left_percent, left, "left")
-            user_nodes_right = self.get_random_nodes_from_labels(right_percent, right, "right")
+            user_nodes_left = self.get_random_nodes(left_percent, left)
+            user_nodes_right = self.get_random_nodes(right_percent, right)
 
             user_nodes_left_list = list(user_nodes_left.keys())
             for i in range(len(user_nodes_left_list)-1):
@@ -74,74 +69,48 @@ class RandomWalkControversy(ControversyMeasure):
                 elif side == "right":
                     right_right += 1
 
-        e1 = left_left * 1.0 / (left_left+right_left)
-        e2 = left_right * 1.0 / (left_right+right_right)
-        e3 = right_left * 1.0 / (left_left+right_left)
-        e4 = right_right * 1.0 / (left_right+right_right)
-        rwc = e1*e4 - e2*e3
+        e1 = left_left / (left_left + right_left)
+        e2 = left_right / (left_right + right_right)
+        e3 = right_left / (left_left + right_left)
+        e4 = right_right / (left_right + right_right)
+        rwc = round(e1*e4 - e2*e3, 2)
 
         border_msg("Random Walk Controversy: {}".format(rwc))
         return rwc
 
     @staticmethod
-    def get_random_nodes_from_labels(k, side, flag):
+    def get_random_nodes(k, side):
 
         random_nodes = []
         random_nodes_dict = {}
 
-        if flag == "left":
-            for i in range(k):
-                random_num = random.randint(0, len(side)-1)
-                random_nodes.append(side[random_num])
+        for i in range(k):
 
-        elif flag == "right":
-            for i in range(k):
-                random_num = random.randint(0, len(side)-1)
-                random_nodes.append(side[random_num])
-
-        else:
-            for i in range(k/2):
-                random_num = random.randint(0, len(side)-1)
-                random_nodes.append(side[random_num])
-
-            for i in range(k/2):
-                random_num = random.randint(0, len(side)-1)
-                random_nodes.append(side[random_num])
-
-        for ele in random_nodes:
-            random_nodes_dict[ele] = 1
+            random_num = random.randint(0, len(side)-1)
+            random_nodes.append(side[random_num])
+            random_nodes_dict[side[random_num]] = 1
 
         return random_nodes_dict
 
     @staticmethod
-    def perform_random_walk(graph, starting_node, user_nodes_side1, user_nodes_side2):
+    def perform_random_walk(graph, node, left_side, right_side):
 
         flag = 0
         side = ""
-        node_seen = dict()
-        neighbors = list(graph.neighbors(starting_node))
-        num_neighbors = len(neighbors)
+        starting_node = node
 
         while flag != 1:
 
+            neighbors = list(graph.neighbors(starting_node))
             random_num = random.randint(0, len(neighbors)-1)
-            neighbor = neighbors[random_num]
+            starting_node = neighbors[random_num]
 
-            if neighbor in node_seen:
-                neighbors.remove(neighbor)
-                node_seen[neighbor] += 1
-            else:
-                node_seen[neighbor] = 1
-
-            if neighbor in user_nodes_side1:
+            if starting_node in left_side:
                 side = "left"
                 flag = 1
 
-            elif neighbor in user_nodes_side2:
+            elif starting_node in right_side:
                 side = "right"
                 flag = 1
-
-            elif len(node_seen) == num_neighbors:
-                break
 
         return side
